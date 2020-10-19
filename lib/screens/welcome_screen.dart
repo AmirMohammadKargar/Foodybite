10dbce7b975aa0aeec2aa7b3ae0e65b8164f4e9f
AmirMohammadKargar <a.m.kargar2000@gmail.com>
2020-10-19 11:59:06 +0330

    Complete Welcome screen.

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,10 +1,61 @@
+import 'package:Foodybite/widgets/circle_next_button.dart';
 import 'package:flutter/material.dart';
 
-class WelcomeScreen extends StatelessWidget {
-  const WelcomeScreen({Key key}) : super(key: key);
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({Key key, t}) : super(key: key);
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  bool _visible = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    Future.delayed(const Duration(milliseconds: 1000), () {
+      setState(() {
+        _visible = true;
+      });
+    });
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            child: Image(
+              image: AssetImage('assets/images/Welcome.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          Positioned(
+            top: 370,
+            left: 100,
+            child: AnimatedOpacity(
+              opacity: _visible ? 1.0 : 0.0,
+              duration: Duration(seconds: 3),
+              child: Text(
+                'Foodybite',
+                style: TextStyle(
+                  fontSize: 55.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 460,
+            left: 190,
+            child: AnimatedOpacity(
+              opacity: _visible ? 1.0 : 0.0,
+              duration: Duration(seconds: 5),
+              child: CircleNextButton(
+                onPress: () {},
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
