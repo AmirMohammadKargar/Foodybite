4f8bc284aa646d68009771caf426caa9995d2d23
AmirMohammadKargar <a.m.kargar2000@gmail.com>
2020-10-21 11:15:16 +0330

    Create small text box for cards.

---
+++ lib/widgets/small_text_box.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class SmallTextBox extends StatelessWidget {
+  const SmallTextBox({
+    Key key,
+    this.title,
+    this.color,
+  }) : super(key: key);
+  final String title;
+  final Color color;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(3.0),
+      ),
+      width: 50,
+      height: 20,
+      child: Center(
+        child: Text(
+          '$title',
+          style: TextStyle(
+            color: color,
+            fontSize: 13.0,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
