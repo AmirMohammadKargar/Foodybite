38e97970de4c1822f4aca3bb590e30f952b289a4
AmirMohammadKargar <a.m.kargar2000@gmail.com>
2020-10-27 08:37:03 +0330

    Fix bug and add find screen navigator to home screen.

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -26,59 +26,63 @@ class _HomeScreenState extends State<HomeScreen> {
       bottomNavigationBar: BottomNavigation(),
       backgroundColor: Colors.grey[100],
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            SizedBox(height: 15),
-            Center(
-              child: Search(
-                title: 'Search resturant',
-                icon: Icon(
-                  Icons.search_outlined,
-                  color: Colors.grey,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: 15),
+              Center(
+                child: Search(
+                  title: 'Search resturant',
+                  icon: Icon(
+                    Icons.search_outlined,
+                    color: Colors.grey,
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 35),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20.0,
+              SizedBox(height: 35),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20.0,
+                ),
+                child: RowTitleCraousel(
+                  title: 'Trending Restaurants',
+                  count: 15,
+                  onPress: () {
+                    Navigator.pushNamed(context, '/trendResturantScreen');
+                  },
+                ),
               ),
-              child: RowTitleCraousel(
-                title: 'Trending Restaurants',
-                count: 15,
-                onPress: () {
-                  Navigator.pushNamed(context, '/trendResturantScreen');
-                },
+              SizedBox(height: 20),
+              TrendingResturantCarousel(),
+              SizedBox(height: 25.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20.0,
+                ),
+                child: RowTitleCraousel(
+                  title: 'Category',
+                  count: 9,
+                  onPress: () {},
+                ),
               ),
-            ),
-            SizedBox(height: 20),
-            TrendingResturantCarousel(),
-            SizedBox(height: 25.0),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20.0,
+              CategoryCarousel(),
+              SizedBox(height: 0.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20.0,
+                ),
+                child: RowTitleCraousel(
+                  title: 'Friends',
+                  count: 9,
+                  onPress: () {
+                    Navigator.pushNamed(context, '/findFriendScreen');
+                  },
+                ),
               ),
-              child: RowTitleCraousel(
-                title: 'Category',
-                count: 9,
-                onPress: () {},
-              ),
-            ),
-            CategoryCarousel(),
-            SizedBox(height: 0.0),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20.0,
-              ),
-              child: RowTitleCraousel(
-                title: 'Friends',
-                count: 9,
-                onPress: () {},
-              ),
-            ),
-            FriendsCaresoul(),
-          ],
+              FriendsCaresoul(),
+            ],
+          ),
         ),
       ),
     );
