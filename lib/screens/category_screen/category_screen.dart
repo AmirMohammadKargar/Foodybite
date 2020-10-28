d71d78e8c3aeb0094c7b312a3e5efda4e2635449
AmirMohammadKargar <a.m.kargar2000@gmail.com>
2020-10-28 10:48:38 +0330

    Create category screen.

---
+++ lib/screens/category_screen/category_screen.dart
@@ -0,0 +1,52 @@
+import 'package:Foodybite/screens/category_screen/widgets/category_list.dart';
+import 'package:flutter/material.dart';
+
+class CategoryScreen extends StatelessWidget {
+  const CategoryScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Category',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20.0,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              color: Colors.black,
+            ),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: ListView.builder(
+          itemCount: 8,
+          itemBuilder: (BuildContext context, int index) {
+            return CategoryList(
+              title: 'Italina',
+              color1: Colors.pink,
+              color2: Colors.yellow,
+              image: 'assets/images/italian.png',
+            );
+          }),
+    );
+  }
+}
