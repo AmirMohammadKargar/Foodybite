5bbd185245c1adbae8467c20ce85b8cc8eb61806
AmirMohammadKargar <a.m.kargar2000@gmail.com>
2020-10-30 08:38:48 +0330

    Add onPress function to avatar widgets.

--- lib/screens/home_screen/widgets/friends_crasouel.dart
+++ lib/screens/home_screen/widgets/friends_crasouel.dart
@@ -3,27 +3,33 @@ import 'package:flutter/material.dart';
 class FriendsCaresoul extends StatelessWidget {
   const FriendsCaresoul({
     Key key,
+    this.onPress,
   }) : super(key: key);
-
+  final Function onPress;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height * 0.1,
-      width: MediaQuery.of(context).size.width,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: 5,
-        itemBuilder: (BuildContext context, int index) {
-          return Padding(
-            padding: const EdgeInsets.only(
-              left: 15.0,
-            ),
-            child: CircleAvatar(
-              backgroundImage: AssetImage('assets/images/profile1.png'),
-              radius: 35.0,
-            ),
-          );
-        },
+    return GestureDetector(
+      onTap: () {
+        onPress();
+      },
+      child: Container(
+        height: MediaQuery.of(context).size.height * 0.1,
+        width: MediaQuery.of(context).size.width,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: 5,
+          itemBuilder: (BuildContext context, int index) {
+            return Padding(
+              padding: const EdgeInsets.only(
+                left: 15.0,
+              ),
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/images/profile1.png'),
+                radius: 35.0,
+              ),
+            );
+          },
+        ),
       ),
     );
   }
