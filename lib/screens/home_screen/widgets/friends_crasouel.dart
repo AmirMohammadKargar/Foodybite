43f8c5f636eb6180efe31be83b968456c4c06212
AmirMohammadKargar <a.m.kargar2000@gmail.com>
2020-10-31 08:13:46 +0330

    Add user model.

--- lib/screens/home_screen/widgets/friends_crasouel.dart
+++ lib/screens/home_screen/widgets/friends_crasouel.dart
@@ -1,35 +1,42 @@
+import 'package:Foodybite/models/users.dart';
 import 'package:flutter/material.dart';
 
 class FriendsCaresoul extends StatelessWidget {
   const FriendsCaresoul({
     Key key,
-    this.onPress,
   }) : super(key: key);
-  final Function onPress;
+
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        onPress();
-      },
-      child: Container(
-        height: MediaQuery.of(context).size.height * 0.1,
-        width: MediaQuery.of(context).size.width,
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: 5,
-          itemBuilder: (BuildContext context, int index) {
-            return Padding(
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.1,
+      width: MediaQuery.of(context).size.width,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 7,
+        itemBuilder: (BuildContext context, int index) {
+          Users user = users[index];
+          return GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, '/profileScreen', arguments: {
+                'name': user.name,
+                'imageUrl': user.imageUrl,
+                'following': user.following.toString(),
+                'follower': user.follower.toString(),
+                'email': user.email,
+              });
+            },
+            child: Padding(
               padding: const EdgeInsets.only(
                 left: 15.0,
               ),
               child: CircleAvatar(
-                backgroundImage: AssetImage('assets/images/profile1.png'),
+                backgroundImage: AssetImage(user.imageUrl),
                 radius: 35.0,
               ),
-            );
-          },
-        ),
+            ),
+          );
+        },
       ),
     );
   }
